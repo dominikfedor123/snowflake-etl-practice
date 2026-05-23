7c19245d6975d36556a489fc508fafe44c8c480b
Dominik Fedor <dominikfedor17@gmail.com>
2026-05-23 17:12:19 +0200

    added dq check to customers staging

--- 04_staging_customers.sql
+++ 04_staging_customers.sql
@@ -1,4 +1,5 @@
 create or replace table staging.customers_clean
-as select distinct customer_id
+as select distinct try_to_number(customer_id) as customer_id,
+case when try_to_number(customer_id) is not null then 1 else 0 end as dq_customer_id
 from raw.orders
 where customer_id is not null
